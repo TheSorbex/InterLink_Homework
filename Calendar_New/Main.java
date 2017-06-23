642945f9b3e4fd7bfdeb77c5ba6cd5ba10e6048f
Alexey Yavnyk <thesorbex@gmail.com>
2017-06-23 12:36:34 +0300

    update 2.0

--- Calendar_New/Main.java
+++ Calendar_New/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Main {
 
@@ -9,32 +11,37 @@ public class Main {
         LocalDate date = LocalDate.now();
         int month = date.getMonthValue();
         int today = date.getDayOfMonth();
+        TextStyle text = TextStyle.SHORT;
+        Locale locale = Locale.ENGLISH;
+
 
         date = date.minusDays(today - 1);
         DayOfWeek weekday = date.getDayOfWeek();
         int value = weekday.getValue();
 
-        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (DayOfWeek day : DayOfWeek.values()){
+            System.out.print(day.getDisplayName(text, locale)+ " ");
+        }
+
+        System.out.println();
+
         for(int i = 0; i < value; i++) {
             System.out.print("   ");
         }
         while (date.getMonthValue() == month){
             System.out.print(writeDayWithColor(date,today));
             date = date.plusDays(1);
-            if (date.getDayOfWeek().getValue() == 1){
+            if (date.getDayOfWeek() == DayOfWeek.MONDAY){
                 System.out.println();
             }
             }
-        if (date.getDayOfWeek().getValue() != 1) {
-            System.out.println();
-        }
     }
 
     public static String writeDayWithColor(LocalDate date, int today){
         String color;
         String spaces;
 
-        if (date.getDayOfMonth() + 1 < 11){
+        if (date.plusDays(1).getDayOfMonth() < 11){
             spaces = "   ";
         } else {
             spaces = "  ";
@@ -42,7 +49,7 @@ public class Main {
 
         if (date.getDayOfMonth() == today){
             color = "34";
-        } else if (date.getDayOfWeek().getValue() % 7 != 6 && date.getDayOfWeek().getValue() % 7 != 0){
+        } else if (date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY){
             color = "30";
         } else {
             color = "31";
